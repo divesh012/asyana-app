1a4492d7480bdca776e5e4701084c25fcaad9e48
Divesh Kuthe <151843097+divesh012@users.noreply.github.com>
2026-04-12 16:06:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def create_order():
     return jsonify(order)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
