480b74807eb3c2922e46f414cec23d5382319f8f
Divesh Kuthe <151843097+divesh012@users.noreply.github.com>
2026-07-25 22:42:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,6 +80,6 @@ def payment_success():
             "message": str(e)
         }), 400
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
